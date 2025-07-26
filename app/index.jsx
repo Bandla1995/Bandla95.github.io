6c0d8f3194be3254fc0f85f8c232dca6f206fcab
Benjamin Khonye <bkhonye@gmail.com>
2025-07-26 08:16:24 +0200

    files were added and the pages were linked to shift among each other.

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
+import { Link } from 'expo-router'
+import home from '../assets/img/home.png'
 
 const Home = () => {
   return (
     <View style={styles.container}>
-      <Text>The Number 1</Text>
-      <Text>Reading List App</Text>
+      <Image source={home} style={styles.img} />
+      <Text style={styles.title}>The Number 1</Text>
+
+      <Text style={{ marginTop: 10, marginBottom: 30 }}>
+        Reading List App
+      </Text>
+
+      <Link href="/about" style={styles.link}>About Page</Link>
     </View>
   )
 }
@@ -15,7 +23,19 @@ export default Home
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems  : 'center',
+    alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 20,
+
+  },
+  img: {
+    marginVertical: 20,
+  },
+   link: {
+        marginVertical: 10,
+        borderBottomWidth: 1,
+  }
 })
